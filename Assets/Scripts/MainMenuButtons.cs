85641b835cdd10ec9128e0137e07a8bc9caf6ac0
Sebastien Feser <sedukafe@gmail.com>
2019-01-25 22:52:16 +0100

    Simple Main Menu done
    Play and Quit buttons. Some other buttons can be easily added in the future

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void Play()
+    {
+        //Load Scene... I can also do a transition, but in the polish
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
