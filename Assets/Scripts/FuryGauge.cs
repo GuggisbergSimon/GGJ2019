0dcbcbb180de76662546f13bd92f960550728680
Sebastien Feser <sedukafe@gmail.com>
2019-01-25 22:30:45 +0100

    HealthBar and death condition

---
+++ Assets/Scripts/FuryGauge.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuryGauge : MonoBehaviour
+{
+    float furyMax = 100;
+    [SerializeField] float furyTime;
+    int fury;
+    [SerializeField] Image furyBar;
+
+    public int Fury
+    {
+        get { return fury; }
+        set { fury = value; }
+    }
+
+    private void Start()
+    {
+        StartCoroutine("DrawFury");
+    }
+
+    IEnumerator DrawFury()
+    {
+        for (fury = 0; fury < furyMax; fury++)
+        {
+            furyBar.fillAmount = fury / furyMax;
+            yield return new WaitForSeconds((1*furyTime)/furyMax);
+        }
+        furyBar.fillAmount = 1;
+    }
+}
