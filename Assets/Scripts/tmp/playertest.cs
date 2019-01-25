5ea2da12295b8d3220794806da610a40cc8bc172
Unknown <lucafloreau@akeonet.com>
2019-01-25 22:10:58 +0100

    door + camera

---
+++ Assets/Scripts/tmp/playertest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playertest : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x+(Input.GetAxis("Horizontal")*speed), transform.position.y + (Input.GetAxis("Vertical")*speed), 0);
+    }
+}
