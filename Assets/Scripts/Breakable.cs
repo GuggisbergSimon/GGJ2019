ab5484fd3102c6330343f861d1f5016bff367bbe
Unknown <slyguggi@gmail.com>
2019-01-25 22:01:36 +0100

    added working player, can break items

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+	[SerializeField] private int resistancePoints = 1;
+	private bool _isBroken = false;
+	
+	public void Damage(int points)
+	{
+		if (!_isBroken)
+		{
+			resistancePoints -= points;
+			if (resistancePoints <= 0)
+			{
+				Debug.Log("Bling");
+				_isBroken = true;
+			}
+		}
+	}
+}
