6d678c145ba946c835f9e77130313cadd2701e4c
Unknown <slyguggi@gmail.com>
2019-01-26 18:56:55 +0100

    added basic pixel scene tutorial

---
+++ Assets/Scripts/AutoLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoLoad : MonoBehaviour
+{
+	[SerializeField] private float minTime = 5.0f;
+	[SerializeField] private float maxTime = 10.0f;
+	[SerializeField] private string sceneToLoad = null;
+
+	private void Start()
+	{
+		Invoke("LoadScene", Random.Range(minTime, maxTime));
+	}
+
+	void LoadScene()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
