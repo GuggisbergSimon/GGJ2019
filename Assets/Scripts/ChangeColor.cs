bda8d914dced722fd68fe15ff538ea78727e4799
Unknown <slyguggi@gmail.com>
2019-01-26 22:05:05 +0100

    polished game over and win scene (with a cool dance)

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+	[SerializeField] private float timeBetweenChangingColor;
+	private SpriteRenderer _mySpriteRenderer;
+
+	private void Start()
+	{
+		_mySpriteRenderer = GetComponent<SpriteRenderer>();
+		StartCoroutine(ChangeHSV());
+	}
+
+	private IEnumerator ChangeHSV()
+	{
+		float h = 0.0f;
+		while (true)
+		{
+			_mySpriteRenderer.color = Color.HSVToRGB(h % 1, 1, 0.5f);
+			h+=0.01f;
+			yield return new WaitForSeconds(timeBetweenChangingColor);
+		}
+	}
+}
